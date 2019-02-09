e4e6a05611061bbf752a6ecaff9a399bf00a24b8
Ian Kigen <ian.Gabe.ian@gmail.com>
2019-02-09 03:36:43 +0300

    chore: add shop

---
+++ shop/settings/production.py
@@ -0,0 +1,17 @@
+from .base import *
+
+DEBUG = False
+
+# Database
+# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+
+DATABASES = {
+	'default': {
+		'ENGINE': 'django.db.backends.postgresql',
+		'NAME': os.getenv('DB_NAME', ''),
+		'USER': os.getenv('DB_USER', ''),
+		'PASSWORD': os.getenv('DB_PASSWORD', ''),
+		'HOST': os.getenv('DB_HOST', ''),
+		'PORT': os.getenv('DB_PORT', ''),
+	}
+}
