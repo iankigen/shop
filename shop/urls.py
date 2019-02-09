cdee351a49990cc330897de71e9edc2083d6fb48
Ian Kigen <ian.Gabe.ian@gmail.com>
2019-02-09 17:41:37 +0300

    chore: add users urls

--- shop/urls.py
+++ shop/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework_jwt.views import refresh_jwt_token
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+	path('api/v1/', include('users.urls')),
+	path('admin/', admin.site.urls),
+	path('api-token-refresh/', refresh_jwt_token),
 ]
